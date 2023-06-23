f7248d7d726aedf4875be3792ced32f2e20e3a9b
jc-gi <juancarlos.gonzalez@upslp.edu.mx>
2023-06-22 19:25:10 -0600

    Corregido Jump 0

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/* using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +11,115 @@ public class Controller : MonoBehaviour
     [SerializeField] float speedRampUpTime = 1f;
     private float speedRampTimer = 0f;
 
+    public KeyCode jumpKey = KeyCode.Space; // Tecla que activa el salto y el Wall Kick
+
+    private bool canJump = true; // Indica si el personaje puede realizar un salto
+    private bool isWallKickEnabled = false; // Indica si el personaje puede realizar Wall Kick
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0.2f;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask wallLayer;
+    private bool isGrounded;
+    private bool isTouchingWall;
+
+    Animator myAnimator;
+    Rigidbody2D myRigidBody2D;
+
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        CheckGrounded();
+        CheckWallTouch();
+        Movement();
+        Jump();
+    }
+
+    void Movement()
+    {
+        float CurrentMovement = Input.GetAxis("Horizontal");
+
+        if (CurrentMovement != 0)
+        {
+            speedRampTimer += Time.deltaTime;
+            float currentSpeed = Mathf.Lerp(walkSpeed, runSpeed, speedRampTimer / speedRampUpTime);
+
+            // Set your speed parameter here.
+            myAnimator.SetFloat("Speed", currentSpeed);
+
+            transform.Translate(new Vector2(CurrentMovement * currentSpeed * Time.deltaTime, 0));
+        }
+        else
+        {
+            speedRampTimer = 0f;
+            myAnimator.SetFloat("Speed", 0f);
+        }
+    }
+
+    void Jump()
+    {
+        if (isGrounded && Input.GetKeyDown(jumpKey) && canJump)
+        {
+            myAnimator.SetTrigger("Jump");
+            myRigidBody2D.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+            canJump = false;
+        }
+        else if (isWallKickEnabled && Input.GetKeyDown(jumpKey))
+        {
+            myAnimator.SetTrigger("Jump");
+            myRigidBody2D.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        }
+    }
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (isGrounded)
+        {
+            myAnimator.SetBool("Land", false);
+            canJump = true;
+        }
+    }
+
+    void CheckWallTouch()
+    {
+        isTouchingWall = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, wallLayer);
+
+        if (isTouchingWall)
+        {
+            isWallKickEnabled = true;
+        }
+        else
+        {
+            isWallKickEnabled = false;
+        }
+    }
+}
+
+ */
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float walkSpeed = 3f;
+    [SerializeField] float runSpeed = 6f;
+    [SerializeField] float speedRampUpTime = 1f;
+    private float speedRampTimer = 0f;
+    [SerializeField] float groundDistance = 0.6f; // Puedes ajustar este valor según sea necesario
+
     //WALL KICK
     public float wallKickForce = 5f; // Fuerza del impulso de Wall Kick
     public KeyCode wallKickKey = KeyCode.Space; // Tecla que activa el Wall Kick
@@ -18,7 +127,7 @@ public class Controller : MonoBehaviour
     
 
     [SerializeField] Transform groundCheck;
-    [SerializeField] float groundCheckRadius = 0.2f;
+    //[SerializeField] float groundCheckRadius = 0.2f;
     [SerializeField] LayerMask groundLayer;
     public bool isGrounded;
 
@@ -95,12 +204,25 @@ private void PerformWallKick()
 
 void CheckGrounded()
 {
-    isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    // Crea un raycast hacia abajo desde la posición del personaje
+    RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, groundDistance, groundLayer);
+
+    // Dibuja el raycast en el editor de Unity
+    Debug.DrawRay(transform.position, -Vector2.up * groundDistance, Color.red);
+
+    // Si el raycast golpea algo, entonces el personaje está en el suelo
+    isGrounded = hit.collider != null;
 
     if (isGrounded)
     {
         myAnimator.SetBool("Land", false);
     }
+    /* isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+    if (isGrounded)
+    {
+        myAnimator.SetBool("Land", false);
+    } */
 }
 
  private void OnCollisionEnter2D(Collision2D collision)
